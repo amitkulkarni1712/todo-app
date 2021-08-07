ae0e77d876269fe209033ecd3f42eb2fb53c6621
amitkulkarni1712 <a.kulkarni888@gmail.com>
2021-08-07 19:09:48 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -4,7 +4,7 @@
 #     3. pip install -r /requirements.txt
 #     4. python todo.py
 #     5. hit on browser http://127.0.0.1:5000/health
-
+import datetime
 from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
@@ -12,15 +12,19 @@ app = Flask(__name__)
 tasks = [
     {
         'id': 1,
-        'title': 'Buy groceries',
-        'description': 'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
+        'task_name': 'Buy groceries',
+        'description': 'Chitale_Milk, Amul_Cheese, Pizza_base, apple_Fruit',
+        'done': False,
+        'created_date': "",
+        'updated_date': ""
     },
     {
         'id': 2,
-        'title': 'Learn golang',
+        'task_name': 'Learn golang',
         'description': 'Need to find a good golang tutorial on the web',
-        'done': False
+        'done': False,
+        'created_date': "",
+        'updated_date': "" 
     }
 ]
 
@@ -35,28 +39,48 @@ def health_check():
     return jsonify({'status': 'Up'})
 
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route('/task', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
 
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+@app.route('/task/<int:task_id>', methods=['GET'])
 def get_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     return jsonify({'task': task[0]})
 
+@app.route('/task/<int:task_id>', methods=['PUT'])
 
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'task_name' in request.json and type(request.json['task_name']) is not str:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not str:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['task_name'] = request.json.get('task_name', task[0]['task_name'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    task[0]["updated_date"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return jsonify({'task': task[0]}), 201
+
+@app.route('/task', methods=['POST'])
 def create_task():
-    if not request.json or not 'title' in request.json:
+    if not request.json or not 'task_name' in request.json:
         abort(400)
     task = {
         'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
+        'task_name': request.json['task_name'],
         'description': request.json.get('description', ""),
-        'done': False
+        'done': False,
+        'created_date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
     tasks.append(task)
     return jsonify({'task': task}), 201
@@ -68,3 +92,5 @@ def create_app():
 
 if __name__ == '__main__':
     create_app()
+
+
